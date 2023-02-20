bf1bfc19dfff5c04a8da14a2386c83f2e09a0bd1
Rishabh <rishabhaj13@gmail.com>
2023-02-20 13:53:12 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        rw.setName("Rishabh");
+
+        System.out.println(rw.getName());
+    }
   
 }
